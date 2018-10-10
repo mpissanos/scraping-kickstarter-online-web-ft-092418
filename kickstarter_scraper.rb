3a9bd9a57c7eec022cd984789a9406e4c9c357fb
Manny Pissanos <mpissano@gmail.com>
2018-10-10 18:10:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,11 +11,11 @@ def create_project_hash
   projects = {}
   kickstarter.css('li.project.grid_4').each do
     |project| 
-      title = project.css()
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+        image_link: 
+      }
   end
   projects
-  
-    
 end
 
-create_project_hash
